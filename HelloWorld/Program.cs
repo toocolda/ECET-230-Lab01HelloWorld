5a6f679dedc7dcf4a287762d87ebe449c943393c
c0489342 <c0489342@TB205A-10>
2021-09-09 15:50:22 -0700

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nWhat is your name?");
+            var username = Console.ReadLine();
+            Console.WriteLine($"\nHello {username}!");
+
+            int dayOfYear = DateTime.Now.DayOfYear;
+
+            Console.Write("Day of year: ");
+            Console.WriteLine(dayOfYear);
+
+        }
+    }
+}
